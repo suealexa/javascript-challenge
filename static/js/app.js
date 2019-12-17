9d80a2ad8a3f391005d886ecf20d132cc85e987b
Susan_Roedl <suealexa@hotmail.com>
2019-12-16 21:36:36 -0600

    done

--- static/js/app.js
+++ static/js/app.js
@@ -10,7 +10,7 @@ var tbody = d3.select("tbody");
 // sightings values (date)
  ufoData.forEach(function(sightings) {
   console.log(sightings);
- 
+  
   var row = tbody.append("tr");
 Object.entries(sightings).forEach(function([key, value]) {
 console.log(key, value);
@@ -24,19 +24,49 @@ cell.text(value);
 
 // Select the button
 var button = d3.select("#filter-btn");
+var button1 = d3.select("#filter-btn cities");
+//var button2 = d3.select("#filter-btn states");
 button.on("click", function() {
 
     // Select the input element and get the raw HTML node
     var searchDate = d3.select("#datetime");
-  
+    var searchCity = d3.select("#city");
+    //var searchState = d3.select("#state");
+
     // Get the value property of the input element
     var inputValue = searchDate.property("value");
-  
-    console.log(inputValue);
-    
+    var inputValue = searchCity.property("value");
+    //var inputValue = searchState.property("value");
+
     var filteredData = ufoData.filter(event => event.datetime === inputValue);
+    console.log(inputValue);
   
+    var filteredData = ufoData.filter(event => event.city === inputValue);
+    //var filteredData = ufoData.filter(event => event.state === inputValue);
+
     console.log(filteredData);
+    tbody.html("");
+    filteredData.forEach(function(event)
+    {
+      console.log(event)
+        var row = tbody.append("tr");
+        Object.entries(event).forEach(function([key, value]) {
+            console.log(key, value);
+
+     // Append a cell to the row for each value in the weather report object
+   var cell = row.append("td");
+     cell.text(value);
+
+});
+    });
+});
+
+var button = d3.select("#filter-btn city");
+button.on("click", function() {
+var searchCity = d3.select("#city");
+var inputValue = searchCity.property("value");
+var filteredData = ufoData.filter(event => event.city === inputValue);
+console.log(filteredData);
     tbody.html("");
     filteredData.forEach(function(event)
     {
